2ea46b42c368f12c64d332738a30eebf3a3eab61
Goden <goden.incredible@gmail.com>
2016-11-21 02:59:49 +0800

    管理React元件內部狀態

--- TodoItem.js
+++ TodoItem.js
@@ -1,17 +1,65 @@
+const {InputField} = window.App;
+
 class TodoItem extends React.Component {
+
+	constructor(props, context) {
+		super(props, context);
+
+		// 利用類別建構子初始元件狀態
+		this.state = { editable: false };
+
+		// 在ES6 component class中，必須手動綁定this
+		this.toggleEditMode = this.toggleEditMode.bind(this);
+	}
+
+	// 更新元件狀態來初換模式
+	toggleEditMode() {
+		this.setState({editable: !this.state.editable});
+	}
+
+	test() {
+		console.log(111);
+	}
+
+	// 根據元件狀態來渲染不同畫面
 	render() {
+		return this.state.editable ?
+					this.renderEditMode() :
+					this.renderViewMode();
+	}
+
+	renderViewMode() {
 		const {
-			title, 
+			title,
 			complete
 		} = this.props;
+
 		return (
 				<div>
 					<input type="checkbox" checked={complete} />
-					{title}
+					<span onClick={this.toggleEditMode}>{title}</span>
 					<button>X</button>
 				</div>
-		)
+		);
 	}
+
+	renderEditMode() {
+			return (
+				<InputField
+					autoFocus
+					placeholder="編輯待辦事項"
+					value={this.props.title}
+					onBlur={this.toggleEditMode}
+					onKeyDown={(e) => {
+							if (e.keyCode === 27) {
+								e.preventDefault();
+								this.toggleEditMode();
+							}
+					}}
+				/>
+			);
+	}
+
 }
 
 window.App.TodoItem = TodoItem;
